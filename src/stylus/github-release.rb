2cd1ae477e1ac0ba35c241fa1d5f0c4a08a562cd
Keita Okamoto <ok@aaa.email.ne.jp>
2016-05-22 21:01:19 +0900

    Add circle ci -> github release deployment settings.
    Update Electron to 1.1.0.

---
+++ src/stylus/github-release.rb
@@ -0,0 +1,28 @@
+require 'json'
+package_json = JSON.parse(File.read('./package.json'))
+version = "v#{package_json['version']}"
+user_name = package_json['config']['userName']
+package_name = package_json['config']['packageName']
+targets = ['osx']
+release_info = "-u #{user_name} -r #{package_name} -t #{version}"
+
+Dir.chdir(File.dirname(__FILE__)) do
+  `github-release info #{release_info}`
+  current_version_exist = $?.exitstatus == 0
+  if current_version_exist
+    STDERR.puts '!!! There has been a release with the same version !!!'
+    exit 1
+  end
+  %x(
+    github-release release #{release_info} \
+                           -n "#{version}" \
+                           --pre-release
+  )
+  targets.each do |target|
+    %x(
+      github-release upload  #{release_info} \
+                             -n "#{target}.zip" \
+                             -f ./dist/#{target}/POET-#{target}.zip
+    )
+  end
+end
